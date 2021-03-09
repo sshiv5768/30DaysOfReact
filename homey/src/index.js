4d3981ab21b49db0a2fe9dba2353d1a9dcf0b637
Shivlal <sshivlal9601@gmail.com>
2021-03-09 16:56:14 +0530

    Javascript ES6 Concepts

---
+++ homey/src/index.js
@@ -0,0 +1,14 @@
+// Javascript ES6 Concepts
+
+// 1: Let keyword
+for(let i =0; i<8; i++){
+    console.log(i);
+}
+
+// 2: Object methods
+const person = {
+    name: 'jagu',
+    walk() {console.log("Walking")}
+}
+
+person.walk();
