2c8a7368be99abc579e1b8a01881f4a1a951d7c6
Shivlal <sshivlal9601@gmail.com>
2021-03-11 23:56:43 +0530

    Html to Pdf converter using jspdf

--- homey/src/index.js
+++ homey/src/index.js
@@ -1,14 +1,48 @@
-// Javascript ES6 Concepts
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {jsPDF} from 'jspdf';
 
-// 1: Let keyword
-for(let i =0; i<8; i++){
-    console.log(i);
+const htmltopdf = e =>{
+    e.preventDefault()
+    let doc = new jsPDF('landscape', 'pt', 'A4');
+    doc.html(document.getElementById('form'), {
+        callback: () =>{
+            doc.save('test.pdf');
+        }
+    });
 }
+const element = (
+    
+    <form action='#'method='GET' id='form'>
+        <label>Buyer's name: </label>
+        <input type='text' name='bname'></input>
+        <br></br>
+        <label>Supplier's name: </label>
+        <input type='text' name='sname'></input>
+        <br></br>
+        <label>Quality</label>
+        <input type='text' name='quality'></input>
+        <br></br>
+        <label>Rate:</label>
+        <input type='text' name='rate'></input>
+        <br></br>
+        <label>Payment Terms</label>
+        <input type='text' name='payday'></input>
+        <br></br>
+        <label>Deliver Time</label>
+        <input type='text' name='day'></input>
+        <br></br>
+        <label>Transport name</label>
+        <input type='text' name='tname'></input>
+        <br></br>
+        <label>Remark</label>
+        <input type='text' name='remark'></input>
+        <br></br>
+        <button onClick={htmltopdf}>Submit</button>
 
-// 2: Object methods
-const person = {
-    name: 'jagu',
-    walk() {console.log("Walking")}
-}
+    </form>
 
-person.walk();
+);
+
+
+ReactDOM.render(element, document.getElementById('root'));
